855157867044eb14e43c7443de67882f8a860680
Jon Markoff <jmarkoff@google.com>
2016-04-25 16:48:23 -0400

    Added red heart and imagebutton to heartfragment. The HeartFragment has state now, still needs to actually start the service.

--- wear/src/main/java/com/example/google/whererunner/HeartFragment.java
+++ wear/src/main/java/com/example/google/whererunner/HeartFragment.java
@@ -1,18 +1,36 @@
 package com.example.google.whererunner;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
+import android.view.*;
+import android.widget.ImageButton;
 
 import com.example.google.whererunner.framework.WearableFragment;
 
 public class HeartFragment extends WearableFragment {
 
+    private static final String TAG = "HeartFragment";
+
+    ImageButton heartButton;
+    boolean heartRateSensorOn = false;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_heart, container, false);
-
+        heartButton = (ImageButton)view.findViewById(R.id.heart_button);
+        heartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                if(heartRateSensorOn){
+                    ((ImageButton) v).setImageResource(R.drawable.ic_heart);
+                }else{
+                    ((ImageButton) v).setImageResource(R.drawable.ic_heart_red);
+                }
+                heartRateSensorOn = !heartRateSensorOn;
+                Log.v(TAG, "Heart Rate Sensor is " + (heartRateSensorOn?"ON.":"OFF."));
+                // TODO: Broadcast message to start/stop heart rate monitor service.
+            }
+        });
         return view;
     }
 
