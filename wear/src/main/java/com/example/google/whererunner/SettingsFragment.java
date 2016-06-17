9f057bca26f942e20471b86b7b67ecc02705a735
Paul R. Saxman <saxman@google.com>
2016-06-17 11:39:24 -0700

    Settings clean-up

--- wear/src/main/java/com/example/google/whererunner/SettingsFragment.java
+++ wear/src/main/java/com/example/google/whererunner/SettingsFragment.java
@@ -4,20 +4,28 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.wearable.view.WearableListView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class SettingsFragment extends Fragment {
+
+    private static final String LOG_TAG = SettingsFragment.class.getSimpleName();
+
+    private WearableListViewAdapter mListViewAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
         String[] listItems = view.getResources().getStringArray(R.array.settings_list);
 
+        mListViewAdapter = new WearableListViewAdapter(getContext(), listItems);
+
         WearableListView listView = (WearableListView) view.findViewById(R.id.settings_list);
-        listView.setAdapter(new WearableListViewAdapter(getContext(), listItems));
+        listView.setAdapter(mListViewAdapter);
         listView.setClickListener(new WearableListViewClickListener());
 
         return view;
@@ -27,11 +35,23 @@ public class SettingsFragment extends Fragment {
         @Override
         public void onClick(WearableListView.ViewHolder viewHolder) {
             Integer tag = (Integer) viewHolder.itemView.getTag();
+            String[] listItems = {};
+
+            switch (tag) {
+                case 0:
+                    listItems = getResources().getStringArray(R.array.settings_map_list);
+                    break;
+                case 1:
+                    listItems = getResources().getStringArray(R.array.settings_location_list);
+                    break;
+            }
+
+            mListViewAdapter.mDataset = listItems;
+            mListViewAdapter.notifyDataSetChanged();
         }
 
         @Override
-        public void onTopEmptyRegionClick() {
-        }
+        public void onTopEmptyRegionClick() {}
     }
 
     private static final class WearableListViewAdapter extends WearableListView.Adapter {
@@ -45,6 +65,7 @@ public class SettingsFragment extends Fragment {
 
         public static class ItemViewHolder extends WearableListView.ViewHolder {
             private TextView textView;
+
             public ItemViewHolder(View itemView) {
                 super(itemView);
                 textView = (TextView) itemView.findViewById(R.id.name);
@@ -62,7 +83,6 @@ public class SettingsFragment extends Fragment {
             TextView view = itemHolder.textView;
 
             view.setText(mDataset[position]);
-
             holder.itemView.setTag(position);
         }
 
